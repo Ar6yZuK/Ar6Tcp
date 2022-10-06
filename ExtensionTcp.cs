37cef39d97eadadc2928388b1a5366bad4533b8a
Ar6yZuK <danya.shubin228@gmail.com>
2022-10-06 17:24:21 +0300

    .

---
+++ ExtensionTcp.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace Ar6Tcp
+{
+	public static class ExtensionTcp
+	{
+		public static TcpState GetState(this TcpClient tcpClient)
+		{
+			var foo = IPGlobalProperties.GetIPGlobalProperties()
+			  .GetActiveTcpConnections()
+			  .SingleOrDefault(x => x.LocalEndPoint.Equals(tcpClient.Client.LocalEndPoint) && x.RemoteEndPoint.Equals(tcpClient.Client.RemoteEndPoint));
+			return foo != null ? foo.State : TcpState.Unknown;
+		}
+	}
+}
